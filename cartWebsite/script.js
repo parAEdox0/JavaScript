69a7ca46784bb6de42e52f949b5b29ff2ca3c9ab
Vishal <vishal.chillal@coriolis.co.in>
2024-03-16 19:42:54 +0530

    Updated cart counter and navigation counter

--- cartWebsite/script.js
+++ cartWebsite/script.js
@@ -5,11 +5,61 @@ const cc = document.querySelectorAll(".cartControls p")
 const plus = document.querySelectorAll(".plus")
 const minus = document.querySelectorAll(".minus")
 
-console.log(itemCart, cartControls, cc, plus, minus)
+
+
+let itemList = []
+
+for(let i = 0; i < itemCart.length; i++){
+    itemList[i] = {
+        name: "",
+        count: 0,
+    };
+}
+
+console.log(itemList)
+
+// console.log(itemCart, cartControls, cc, plus, minus)
 
 for(let i = 0; i < itemCart.length; i++){
     itemCart[i].addEventListener("click", () => {
+        navCounter.style.display = `block`
         itemCart[i].style.display = `none`
         cartControls[i].style.display = `flex`
     })
 }
+
+for(let i = 0; i < plus.length; i++){
+    plus[i].addEventListener("click", () => {
+        // console.log(i)
+        itemList[i].count +=1
+        cc[i].innerText = itemList[i].count
+        updateCounter()
+    })
+}
+
+for(let i = 0; i < plus.length; i++){
+    minus[i].addEventListener("click", () => {
+        // console.log(i)
+        if(itemList[i].count > 0){
+            itemList[i].count -= 1
+        }
+        if(itemList[i].count == 0){
+            itemCart[i].style.display = `block`
+            cartControls[i].style.display = `none`
+        }
+        cc[i].innerText = itemList[i].count
+        updateCounter()
+
+    })
+}
+function updateCounter(){
+let counter = 0
+for(let i = 0; i < itemList.length; i++){
+    console.log(i, `=`, itemList[i].count)
+    counter += itemList[i].count
+}
+navCounter.innerText = counter
+}
+
+
+
