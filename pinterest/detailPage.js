c251b478151abf742402b27c490db865914d883a
Parth Ekbote <paraedox0@gmail.com>
2024-03-17 18:50:31 +0530

    modified html and css files of detailPage.html

--- pinterest/detailPage.js
+++ pinterest/detailPage.js
@@ -3,7 +3,7 @@
 const newPageImage = document.querySelector(".newPageImage");
 const clickedImage = localStorage.getItem("clickedImage");
 
-newPageImage.innerHTML = `<img class="w-[90%] rounded-[20px]"
+newPageImage.innerHTML = `<img class="w-[90%] h-[60vh] rounded-[20px]"
 src="${clickedImage}" alt="">`
 
 
