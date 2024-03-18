68c10e30b7d7ead024aea513c7c8939fef9fbd1f
Parth Ekbote <paraedox0@gmail.com>
2024-03-19 00:10:45 +0530

    moved import export javascript files to backEnd repository

--- export.js
+++
@@ -1,5 +0,0 @@
-console.log("In this file we will export a variable and use it in any other js file");
-
-let toExport = 10;
-
-module.exports = toExport;
--- import.js
+++
@@ -1,5 +0,0 @@
-console.log("In this file we will import the varibale which was exported in any other js file and use it");
-
-let imported = require('./export');
-
-console.log(imported)
