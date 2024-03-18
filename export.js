ed072e7fe7a9c5e238576de1b3602de55977c9e8
Parth Ekbote <paraedox0@gmail.com>
2024-03-18 23:43:19 +0530

    import export in js

---
+++ export.js
@@ -0,0 +1,5 @@
+console.log("In this file we will export a variable and use it in any other js file");
+
+let toExport = 10;
+
+module.exports = toExport;
---
+++ import.js
@@ -0,0 +1,5 @@
+console.log("In this file we will import the varibale which was exported in any other js file and use it");
+
+let imported = require('./export');
+
+console.log(imported)
